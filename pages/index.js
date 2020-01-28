3a807960701358a2fb05cf20e611d7bc6e12d92c
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2020-01-28 06:32:11 +0200

    added sabali thought

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,7 @@ const INDEX = () => {
 
 <div className="h-auto w-full justify-right flex flex-wrap items-end p-2">
   <div> <span className="font-default-book cursor-pointer">Earthy</span> Natural Hair Care(Coming Soon)</div>
+  <div> <span className="font-default-book cursor-pointer">Sabali</span> StreetWear (Coming Soon)</div>
 </div>
 
       <Footer>
