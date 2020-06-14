f48aedfa310dcb7eb1c64cb10e396da222a40a53
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2020-06-14 14:53:04 +0200

    updated meta tags

--- components/head.js
+++ components/head.js
@@ -20,18 +20,42 @@ const Head = props => (
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
     <link rel="icon" href="/static/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="/static/index.css" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta property="og:title" content={props.title || ''} />
-    <meta
-      property="og:description"
-      content={props.description || defaultDescription}
-    />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
+
+
+
+  <title>The old new thing</title>
+  <meta name="description" content="The old new thing">
+  <meta name="author" content="Rawk">
+
+<meta property="og:type" content="website" />
+<meta property="og:title" content="The Old New Thing">
+<meta property="og:description" content="The businesss card of today">
+<meta property="og:image" content="https://devuniversity.github.io/dot/card.jpg">
+<meta property="og:url" content="https://devuniversity.github.io/dot/">
+
+<!-- twitter -->
+
+<meta name="twitter:title" content="The Old New Thing">
+<meta name="twitter:description" content="The businesss card of today">
+<meta name="twitter:image" content="https://devuniversity.github.io/dot/card.jpg">
+<meta name="twitter:card" content="summary_large_image">
+
+<!-- fb analytics -->
+<meta property="fb:app_id" content="your_app_id" />
+
+<!-- twitter analytics -->
+<meta name="twitter:site" content="@rawkthethinker">
+
+<!-- fb site name -->
+<meta property="og:site_name" content="The old new thing">
+
+<!-- twitter alt name -->
+
+<meta name="twitter:image:alt" content="The business card of today">
+
+
+<meta name="og:image:alt" content="The business card of today">
+
 
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-89414882-2">
 </script>
